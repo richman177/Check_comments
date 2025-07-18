b2025eae408d0a6b0ed1e1df5118d68b1bdf01d5
richman177 <elmirbektoktoraliev06@gmail.com>
2025-07-18 14:52:21 +0600

    In this project, I built my own artificial intelligence model.

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import joblib
+import uvicorn
+
+model = joblib.load("model_nb.pkl")
+vectorizer = joblib.load("vec.pkl")
+
+app = FastAPI(title="Review Classification API")
+
+
+class ReviewInput(BaseModel):
+    text: str
+
+
+@app.post("/predict")
+def predict_label(review: ReviewInput):
+    try:
+        text_vector = vectorizer.transform([review.text])
+        prediction = model.predict(text_vector)[0]
+        return {"prediction": prediction}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
