1d9b35f4af4003977afe92ed1fb82a3b28fa342f
Elmirbek Toktoraliev <elmirbektoktoraliev06@gmail.com>
2025-07-24 10:58:12 +0600

    Update main.py
    h

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException  
 from pydantic import BaseModel 
 import joblib 
-import uvicorn
+import uvicorn 
 
 model = joblib.load("model_nb.pkl")
 vectorizer = joblib.load("vec.pkl")
