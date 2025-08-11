864aa0a16fd150ca44e6a7a9ef66afb8be6d46d3
Elmirbek Toktoraliev <elmirbektoktoraliev06@gmail.com>
2025-08-11 08:51:25 +0600

    Update main.py
    ew

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ class ReviewInput(BaseModel):
 def predict_label(review: ReviewInput):
     try:
         text_vector = vectorizer.transform([review.text])
-        prediction = model.predict(text_vector)[0]
+        prediction = model.predict(text_vector)[0] 
         return {"prediction": prediction} 
     except Exception as e: 
         raise HTTPException(status_code=500, detail=str(e)) 
